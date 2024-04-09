8cc8f85a545fab6f675f7d75f8bc8af38e940bd3
memorizeTODO <memorizeTODO@gmail.com>
2024-04-09 13:49:00 +0900

    get/weather 문제 수정

--- campscore/src/main/java/com/team5/campscore/utilities/WeatherDataExtractor.java
+++ campscore/src/main/java/com/team5/campscore/utilities/WeatherDataExtractor.java
@@ -18,15 +18,22 @@ public class WeatherDataExtractor {
 		
 
         // 정규 표현식을 사용하여 예시 문자열 패턴을 찾습니다.
-        Pattern pattern = Pattern.compile("[0-9]{2}[A-Z][0-9]{5}\\s+\\d{12}\\s+\\d{12}\\s+A0[0-9]\\s+\\d+\\s+\\d+\\s+WB[0-9]{2}\\s+WB[0-9]{2}\\s+.+?\\s+\"[가-힣]+\"\\s+\\d+");
+        Pattern pattern = Pattern.compile("([0-9]{2}[A-Z][0-9]{5})\\s+(\\d{12})\\s+(\\d{12})\\s+(A0[0-9])\\s+(\\d+)\\s+(\\d+)\\s+(WB[0-9]{2})\\s+(WB[0-9]{2})\\s+(.+)\\s+\"(.+?)\"\\s+(\\d+)");
         Matcher matcher = pattern.matcher(rawData);
 
         // 일치하는 모든 문자열을 저장할 리스트를 생성합니다.
         ArrayList<String> matches = new ArrayList<>();
 
         // 데이터에서 일치하는 모든 문자열을 찾아 리스트에 추가합니다.
+        int loop_cnt=0;
+        int size= 11;
+        String tmp="";
         while (matcher.find()) {
-            matches.add(matcher.group());
+        	tmp="";
+        	for (int i = 1; i <= matcher.groupCount(); i++) {
+        	    tmp=tmp+matcher.group(i).replace(" ","")+" ";
+        	}
+        	matches.add(tmp);
         }
 
         // 리스트를 배열로 변환합니다.
