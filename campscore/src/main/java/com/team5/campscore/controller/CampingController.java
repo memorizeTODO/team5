6c739797cd30a3f70bc03f5607f66a409495ec42
memorizeTODO <memorizeTODO@gmail.com>
2024-04-11 22:51:22 +0900

    .

--- campscore/src/main/java/com/team5/campscore/controller/CampingController.java
+++ campscore/src/main/java/com/team5/campscore/controller/CampingController.java
@@ -14,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -188,6 +190,10 @@ public class CampingController {
 		
 		return new ResponseEntity<>(campingMaps, HttpStatus.OK);
 	}
+	
+	
+	
+	
 	
 	@GetMapping(value ="get/campinglist")
 	ResponseEntity<Map<String, Map<String, Object>>> getCampingToView(@RequestParam Map<String,String> params){
--- campscore/src/main/java/com/team5/campscore/service/LikeDAOImpl.java
+++ campscore/src/main/java/com/team5/campscore/service/LikeDAOImpl.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Service;
 import com.team5.campscore.dao.LikeDAO;
 import com.team5.campscore.model.LikeDTO;
 
-//@Service
+@Service
 public class LikeDAOImpl implements LikeDAO {
-	//@Autowired
+	@Autowired
 	LikeDAO dao;
 	
 	public int insertLike(LikeDTO likeDTO){
@@ -20,9 +20,26 @@ public class LikeDAOImpl implements LikeDAO {
 		}
 		return 1;
 	}
+	public int getCount(LikeDTO likeDTO) {
+		int returnVal;
+		try {
+			returnVal = dao.getCount(likeDTO);
+		}catch(Exception e) {
+			returnVal = -1;//좋아요 갯수가 0일 수도 있으므로 특수하게 실패시 -1를 리턴
+		}
+		return returnVal;
+		
+	}
 	
 	public int deleteLike(LikeDTO likeDTO){
-		
+		try {
+			dao.deleteLike(likeDTO);
+		}catch(Exception e){
+			e.printStackTrace();
+			return 0;
+		}
 		return 1;
 	}
+	
+	
 }
